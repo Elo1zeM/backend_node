943e86991cd439ab0400d8b410119a77b74011fc
Eloize <eloize.muzel@aluno.senai.br>
2025-08-08 14:52:10 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// console.log("Início");
+
+//setTimeout(() =>{
+    //console.log("Esperou 1 segundos...");
+//}, 1000);
+
+//setTimeout(() =>{
+    //console.log("Esperou 2 segundos...");
+//}, 2000);
+
+//console.log("FIM!");
+
+//PROGRAMAÇÂO Assincronan
+
+const somar = require('./SOMA');
+
+console.log(somar(5,3));
+
+//node app.js
